bbd55343dcddeaf6ab51918661b655c604ea8db2
Ynda Jas <yndajas@gmail.com>
2021-03-16 20:51:50 +0000

    commit tree 3927a696850545a6003193c4022069b8fcb11238

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(hhmm) {
+  let hour = parseInt(hhmm.split(0, hhmm.length - 3));
+  if (hour < 12) {
+    return "Good Morning";
+  } else if (hour < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(greeting) {
+  document.querySelector("h1#greeting").innerText = greeting;
+}
